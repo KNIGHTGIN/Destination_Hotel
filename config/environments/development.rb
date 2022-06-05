7a660c6f0aeef60958b03fcc80f212a4fa570cf6
KNIGHTGIN <fumi1101.dragon.and.horse@gmail.com>
2022-06-05 08:33:48 +0000

    デプロイ

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "e3742fea784c40d6a4f5431810385b81.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
+
