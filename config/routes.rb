0efeee1308063413f504e2088157338962982327
KNIGHTGIN <fumi1101.dragon.and.horse@gmail.com>
2022-06-11 07:33:39 +0000

    モデル

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,25 @@ Rails.application.routes.draw do
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "public/homes#top"
+  get "about" => 'public/homes#about'
+
+  scope module: :public do
+    resources :users, only:[:show, :edit, :update, :unsubscribe, :withhdraw] do
+      resources :follows, only:[:create, :destroy]
+    end
+    resources :posts, only:[:index, :show, :new, :create, :edit, :update, :destroy] do
+      resources :likes, only:[:create, :destroy]
+      resources :comments, only:[:create, :destroy]
+      resources :follows, only:[:create, :destroy]
+    end
+  end
+
+  namespace :admin do
+    resources :posts, only:[:index, :new, :create, :show, :edit, :update]
+    resources :tags, only:[:index, :create, :edit, :update]
+    resources :users, only:[:index, :show, :edit, :update]
+  end
+
 end
