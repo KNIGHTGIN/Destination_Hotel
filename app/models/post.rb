106958b1bdf8174e8a7f9da5e85f564205d9eb6b
KNIGHTGIN <fumi1101.dragon.and.horse@gmail.com>
2022-06-18 21:24:57 +0000

    検索機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -29,4 +29,18 @@ class Post < ApplicationRecord
    end
   end
 
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @post = Post.where("hotel_name LIKE?","#{word}")
+    elsif search == "forward_match"
+      @post = Post.where("hotel_name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @post = Post.where("hotel_name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @post = Post.where("hotel_name LIKE?","%#{word}%")
+    else
+      @post = Post.all
+    end
+  end
+
 end
