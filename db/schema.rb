d4329d9098d69c3f9cf7bfeaaa3534b0700f4e15
KNIGHTGIN <fumi1101.dragon.and.horse@gmail.com>
2022-06-21 11:08:46 +0000

    migrate修正

--- db/schema.rb
+++ db/schema.rb
@@ -55,26 +55,14 @@ ActiveRecord::Schema.define(version: 2022_06_17_142107) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "post_tags", force: :cascade do |t|
-    t.integer "post_id", null: false
-    t.integer "tag_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "post"
-    t.string "tag"
-    t.index ["post_id", "tag_id"], name: "index_post_tags_on_post_id_and_tag_id", unique: true
-    t.index ["post_id"], name: "index_post_tags_on_post_id"
-    t.index ["tag_id"], name: "index_post_tags_on_tag_id"
-  end
-
   create_table "posts", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.integer "tag_id"
     t.string "hotel_name"
     t.text "text"
     t.integer "likes_count"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.text "body"
   end
@@ -103,6 +91,4 @@ ActiveRecord::Schema.define(version: 2022_06_17_142107) do
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
-  add_foreign_key "post_tags", "posts"
-  add_foreign_key "post_tags", "tags"
 end
