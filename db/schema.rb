9f458ed04d87f9cf5a0de47cb3b866437805329d
KNIGHTGIN <fumi1101.dragon.and.horse@gmail.com>
2022-07-04 11:22:24 +0000

    モデル修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_18_195010) do
+ActiveRecord::Schema.define(version: 2022_07_04_111730) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -75,14 +75,15 @@ ActiveRecord::Schema.define(version: 2022_06_18_195010) do
     t.integer "likes_count"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "body"
     t.string "name"
+    t.text "body"
   end
 
   create_table "tags", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name", null: false
+    t.integer "tag_id"
   end
 
   create_table "users", force: :cascade do |t|
