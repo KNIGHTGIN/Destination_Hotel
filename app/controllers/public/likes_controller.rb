fd6cb01d57f6e02980687a8cd8094a2735fb30bd
KNIGHTGIN <fumi1101.dragon.and.horse@gmail.com>
2022-07-07 10:02:27 +0000

    いいね非同期

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -1,20 +1,20 @@
 class Public::LikesController < ApplicationController
   before_action :authenticate_user!
   layout 'public/application'
-  
+
   def index
    @like = Like.all
   end
 
   def create
     @like = current_user.likes.create(post_id: params[:post_id])
-    redirect_back(fallback_location: root_path)
+    #redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     @like.destroy
-    redirect_back(fallback_location: root_path)
+    #redirect_back(fallback_location: root_path)
   end
 
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,7 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require jquery
+//= require rails-ujs
 import 'bootstrap';
 import '../stylesheets/application';
