75fcd5403a22c495d76068cf037e46d044a6c3ff
KNIGHTGIN <fumi1101.dragon.and.horse@gmail.com>
2022-08-28 15:07:18 +0000

    追記

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   passwords: 'public/users/passwords'
 }
 
-  root to: "public/homes#top"
+  root to: "public/homes#top" #topページ
   get "about" => 'public/homes#about'
   devise_scope :user do
     post '/users/guest_sign_in', to: 'public/sessions#guest_sign_in' #ゲストログイン
