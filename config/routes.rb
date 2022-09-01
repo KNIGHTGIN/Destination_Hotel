4da6841a27d618fd508cb6d25ac567ba00628ed8
KNIGHTGIN <fumi1101.dragon.and.horse@gmail.com>
2022-09-01 23:59:59 +0900

    COMMIT

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
-  #会員
+  #ユーザー
   devise_for :users, skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions',
